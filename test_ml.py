30a015e5d751b71fdc0e01b83d3b647a1a626830
Curtis <cleav31@wgu.edu>
2024-08-07 19:42:48 -0400

    Added units tests to test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,68 @@
 import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+import numpy as np
+from sklearn.datasets import make_classification
+from ml.model import train_model, inference, compute_model_metrics
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_train_model():
     """
-    # add description for the second test
+    # Test if the train_model function trains a model correctly and checks correct length of predictions
     """
-    # Your code here
-    pass
+    X_train, y_train = make_classification(
+        n_samples = 1000,
+        n_features = 20,
+        n_informative = 2,
+        n_redundant = 10,
+        random_state = 42
+    )
+    model = train_model(X_train, y_train)
+    assert model is not None
+    assert hasattr(model, 'predict')
+
+    # Check for predictions and correct length
+    X_test, _ = make_classification(
+        n_samples = 10,
+        n_features = 20,
+        n_informative = 2,
+        n_redundant = 10,
+        random_state = 42
+    )
+    preds = inference(model, X_test)
+    assert len(preds) == 10
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics():
     """
-    # add description for the third test
+    # Test if the compute_model_metrics function returns correct metrics
     """
-    # Your code here
-    pass
+    y_true = np.array([1, 0, 1, 1, 0, 1, 0, 0, 1, 1])
+    y_pred = np.array([1, 0, 0, 1, 0, 1, 0, 1, 1, 1])
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+    assert precision == pytest.approx(0.8333, rel=1e-3)
+    assert recall == pytest.approx(0.8333, rel=1e-3)
+    assert fbeta == pytest.approx(0.8333, rel=1e-3)
+
+
+def test_inference():
+    """
+    # Test if the inference function returns the correct predictions
+    """
+    X_train, y_train = make_classification(
+        n_samples = 1000,
+        n_features = 20,
+        n_informative = 2,
+        n_redundant = 10,
+        random_state = 42
+    )
+    model = train_model(X_train, y_train)
+
+    X_test, _ = make_classification(
+        n_samples = 10,
+        n_features = 20,
+        n_informative = 2,
+        n_redundant = 10,
+        random_state = 42
+    )
+    preds = inference(model, X_test)
+    assert len(preds) == 10
+    assert all(isinstance(pred, np.integer) for pred in preds)

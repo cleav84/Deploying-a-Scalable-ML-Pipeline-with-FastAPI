70e9962143f9e8249c9782fb2fd1863d76a72304
Curtis <cleav31@wgu.edu>
2024-08-07 20:50:02 -0400

    Flake8 updates

--- train_model.py
+++ train_model.py
@@ -18,7 +18,7 @@ print(data_path)
 data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
-# Optional enhancement, 
+# Optional enhancement,
 # use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(
     data,
@@ -74,7 +74,7 @@ model = load_model(
     model_path
 )
 
-# TODO: use the inference function to 
+# TODO: use the inference function to
 # run the model inferences on the test dataset.
 preds = inference(model, X_test)
 
@@ -82,7 +82,7 @@ preds = inference(model, X_test)
 p, r, fb = compute_model_metrics(y_test, preds)
 print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}")
 
-# TODO: compute the performance on model slices 
+# TODO: compute the performance on model slices
 # using the performance_on_categorical_slice function
 # iterate through the categorical features
 for col in cat_features:
@@ -101,4 +101,9 @@ for col in cat_features:
         )
         with open("slice_output.txt", "a") as f:
             print(f"{col}: {slicevalue}, Count: {count:,}", file=f)
-            print(f"Precision: {p:.4f} | Recall: {r:.4f} | F1: {fb:.4f}", file=f)
+            print(
+                f"Precision: {p:.4f} | "
+                f"Recall: {r:.4f} | "
+                f"F1: {fb:.4f}",
+                file=f
+            )
